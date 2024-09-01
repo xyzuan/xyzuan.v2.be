3db72c95a7bc80546b7037c57c5122d02e3b17b3
xyzuan <jodyyuan@xyzuan.my.id>
2024-09-01 23:32:40 +0700

    fix: Remove api prefix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,8 @@
 import { createElysia } from "@utils/createElysia";
 import { PortfolioController } from "./portfolio/portfolio.controller";
 
-const apiRoutes = createElysia({ prefix: "/api/v2" }).group(
-  "/portfolio",
-  (app) => app.use(PortfolioController)
+const apiRoutes = createElysia({ prefix: "/v2" }).group("/portfolio", (app) =>
+  app.use(PortfolioController)
 );
 
 export default apiRoutes;
