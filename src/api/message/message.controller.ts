9345a0a1d88cd9d635ddab10a4ebdaed4b366d6a
xyzuan <jodyyuan@xyzuan.my.id>
2024-09-09 01:21:31 +0700

    feat(message): Add DELETE message

--- src/api/message/message.controller.ts
+++ src/api/message/message.controller.ts
@@ -2,6 +2,7 @@ import { createElysia } from "@libs/elysia";
 import { t } from "elysia";
 import { authGuard } from "@libs/authGuard";
 import { prismaClient } from "@libs/prismaDatabase";
+import { UnauthorizedException } from "@constants/exceptions";
 
 export const MessageController = createElysia()
   .get(
@@ -53,5 +54,43 @@ export const MessageController = createElysia()
       body: t.Object({
         message: t.String(),
       }),
+    }
+  )
+  .delete(
+    "/:id",
+    async ({ params: { id }, user }) => {
+      const message = await prismaClient.message.findUnique({
+        where: {
+          id: id,
+          userId: user.id,
+        },
+      });
+
+      if (!message) {
+        return {
+          status: 404,
+          data: { message: "Message not found" },
+        };
+      }
+
+      if (message.userId !== user.id && !user.isAdmin) {
+        throw new UnauthorizedException();
+      }
+
+      await prismaClient.message.delete({
+        where: {
+          id: id,
+        },
+      });
+
+      return {
+        status: 200,
+        data: { message: "Message deleted successfully" },
+      };
+    },
+    {
+      detail: {
+        tags: ["Messages"],
+      },
     }
   );
