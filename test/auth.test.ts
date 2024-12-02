2fb2ed88ea3a4a22a7515d36bb300c96e444a8a9
xyzuan <jodyyuan@xyzuan.my.id>
2024-12-02 14:27:09 +0700

    test(): Add more unit test

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "bun:test";
 import { api } from "../src";
+import { generateRandomEmail } from "@utils/mockUtils";
 
 describe("/v2/login Auth Sign In", () => {
   it("If Email / Password cant be null", async () => {
@@ -59,5 +60,87 @@ describe("/v2/login Auth Sign In", () => {
 
     expect(response.status).toBe(400);
     expect(response.body.message).toBe("Password is invalid.");
+  });
+});
+
+describe("/v2/signup Auth Sign Up", () => {
+  it("should return 201 if user can register with valid data", async () => {
+    const response = await api
+      .handle(
+        new Request("http://localhost:3121/v2/auth/signup", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: generateRandomEmail(),
+            password: "password123",
+            name: "Random User",
+          }),
+        })
+      )
+      .then(async (res) => ({
+        status: res.status,
+        body: await res.json(),
+      }));
+
+    expect(response.status).toBe(201);
+  });
+
+  it("should return 422 if email or password is missing", async () => {
+    const response = await api
+      .handle(
+        new Request("http://localhost:3121/v2/auth/signup", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            name: "Test User",
+          }),
+        })
+      )
+      .then(async (res) => ({
+        status: res.status,
+        body: await res.json(),
+      }));
+
+    expect(response.status).toBe(422);
+  });
+
+  it("should return 422 if password is missing", async () => {
+    const response = await api
+      .handle(
+        new Request("http://localhost:3121/v2/auth/signup", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: "test@example.com",
+            name: "Test User",
+          }),
+        })
+      )
+      .then(async (res) => ({
+        status: res.status,
+        body: await res.json(),
+      }));
+
+    expect(response.status).toBe(422);
+  });
+
+  it("should return 422 if email is missing", async () => {
+    const response = await api
+      .handle(
+        new Request("http://localhost:3121/v2/auth/signup", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            password: "password123",
+            name: "Test User",
+          }),
+        })
+      )
+      .then(async (res) => ({
+        status: res.status,
+        body: await res.json(),
+      }));
+
+    expect(response.status).toBe(422);
   });
 });
