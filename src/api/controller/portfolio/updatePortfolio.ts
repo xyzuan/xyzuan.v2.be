a54340ad07e3c9bf2c9b870df150b5c6fdc7c345
xyzuan <jodyyuan@xyzuan.my.id>
2024-12-05 18:42:01 +0700

    opt(portfolio/update): Clear redis key after update

--- src/api/controller/portfolio/updatePortfolio.ts
+++ src/api/controller/portfolio/updatePortfolio.ts
@@ -2,14 +2,16 @@ import { ForbiddenException } from "@constants/exceptions";
 import { authGuard } from "@libs/authGuard";
 import { createElysia } from "@libs/elysia";
 import { prismaClient } from "@libs/prismaDatabase";
+import redis from "@libs/redis";
 import portfolioModel from "@models/portfolio.model";
 
 export default createElysia()
   .use(portfolioModel)
   .use(authGuard)
+  .use(redis)
   .patch(
     "/:id",
-    async ({ body, user, params: { id } }) => {
+    async ({ body, user, params: { id }, redis }) => {
       if (!user.isAdmin) throw new ForbiddenException();
 
       let updatedData: any = body;
@@ -28,13 +30,16 @@ export default createElysia()
           stacks: true,
         },
       });
+
+      await redis.del(`portfolio.${id}`);
+
       return {
         status: 200,
         message: `Portfolio and related stacks updated successfully`,
       };
     },
     {
-      body: "portfolio.model",
+      body: "portfolio.patch.model",
       detail: {
         tags: ["Portfolios"],
       },
--- src/models/portfolio.model.ts
+++ src/models/portfolio.model.ts
@@ -12,6 +12,17 @@ const portfolioModel = new Elysia().model({
     stacks: t.Array(t.String()),
     createdAt: t.Optional(t.Date()),
   }),
+  "portfolio.patch.model": t.Object({
+    img: t.Optional(t.String({ format: "uri" })),
+    title: t.Optional(t.String()),
+    description: t.Optional(t.String()),
+    content: t.Optional(t.String()),
+    href: t.Optional(t.String()),
+    projectLink: t.Optional(t.String({ format: "uri" })),
+    isFeatured: t.Optional(t.Boolean()),
+    stacks: t.Optional(t.Array(t.String())),
+    createdAt: t.Optional(t.Date()),
+  }),
 });
 
 export default portfolioModel;
