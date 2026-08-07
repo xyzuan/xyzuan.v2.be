d706e42c79a469f4605d516c6010c2006a1ea3c5
xyzuan <jodyyuan@xyzuan.com>
2026-08-07 22:13:58 +0700

    fix(): Minio dont use secure SSL

--- src/libs/minioClient.ts
+++ src/libs/minioClient.ts
@@ -4,7 +4,7 @@ const MinioClient = new Minio.Client({
   endPoint: Bun.env.MINIO_HOST!,
   accessKey: Bun.env.MINIO_ACCESS_KEY!,
   secretKey: Bun.env.MINIO_SECRET_KEY!,
-  useSSL: true,
+  useSSL: false,
 });
 
 export default MinioClient;
